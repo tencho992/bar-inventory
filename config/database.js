1279076335ec0d1feb9b9d86999654a0d566e3ac
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-26 16:52:49 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tenzinc:tenzinc@burn-notice.ofi7dnh.mongodb.net/inventory?retryWrites=true&w=majority', 
+    'dbName': 'inventory'
+};
---
+++ public/main.js
@@ -0,0 +1,72 @@
+var plusOne = document.getElementsByClassName("fa-thumbs-up");
+var minusOne = document.getElementsByClassName("fa-thumbs-down");
+
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(plusOne).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const quantity = this.parentNode.parentNode.childNodes[3].innerText
+        const inputValue = parseFloat(this.parentNode.parentNode.childNodes[5].value)
+        console.log(inputValue)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'quantity': quantity,
+            'newQuantity':inputValue
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+Array.from(minusOne).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const quantity = this.parentNode.parentNode.childNodes[3].innerText
+    const minusOne = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    // const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[9].innerText)
+
+    fetch('messages/minusOne', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'quantity': quantity,
+        'plusOne': plusOne
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
